894abc79d9ec04dfcb1663cd3f499ba14a6f3467
gabyceci <20190019@ricaldone.edu.sv>
2025-03-21 09:42:55 -0600

    Inicio de actividad 2

---
+++ gabriela-parte-dos/src/components/about.jsx
@@ -0,0 +1,31 @@
+const About = () => {
+    return (
+      <>
+        <div type="card" className="about"> 
+          <img src="https://cdn.pixabay.com/photo/2018/04/18/18/56/user-3331256_1280.png" class="card-img-top" alt="..." />
+          <div class="card-body">
+          <br />
+
+            <h5 class="card-title">Gabriela Arévalo</h5>
+            <br />
+            <p>
+      Soy una apasionada <strong>desarrolladora de software</strong> con experiencia
+      en la creación de <strong>aplicaciones móviles</strong> y <strong
+        >sitios web</strong
+      >. Me destaco por mi capacidad de adaptación a diferentes áreas del
+      desarrollo, incluyendo <strong>backend</strong>, <strong>diseño</strong> y
+      <strong>sistemas de escritorio</strong>. Mi enfoque <strong
+        >ordenado</strong
+      > y <strong>responsable</strong> me permite afrontar nuevos retos con eficiencia
+      y creatividad.
+    </p>
+
+            
+          </div>
+        </div>
+      </>
+    );
+  };
+  
+  export default About;
+  
